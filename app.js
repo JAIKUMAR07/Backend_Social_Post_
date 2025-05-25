7a56a816a37ecac83a48310946f9182a6393f6f1
JAIKUMAR <jaikumardewangan88@gmail.com>
2025-05-26 01:03:36 +0530

    like edit feature added

--- app.js
+++ app.js
@@ -131,6 +131,34 @@ app.get("/profile", isLoggedIn, async (req, res) => {
     }
 });
 
+app.get("/like/:id" , isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user")  ;
+     
+    if(post.likes.indexOf(req.user.userId) === -1){
+        post.likes.push(req.user.userId);
+    }else {
+        post.likes.splice(post.likes.indexOf(req.user.userId),1);
+    }
+   
+     await post.save();
+
+     
+    res.redirect("/profile");
+});
+
+
+app.get("/edit/:id" , isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user")  ;
+     
+      res.render("edit" , {post : post});
+});
+ 
+
+app.post("/update/:id" , isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id } , {content: req.body.content}) ;
+     
+      res.redirect("/profile"  )
+});
 
 app.post("/post", isLoggedIn, async (req, res) => {
     try {
